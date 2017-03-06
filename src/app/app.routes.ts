4213e469157fbd0aa644ceb827b732bed9b49b74
Grygorii Grygorian <Grygorii_Grygorian@epam.com>
2017-03-06 17:06:43 +0100

    fixed styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home';
 import { NoContentComponent } from './pages/no-content';
+import { CoursesComponent } from './pages/courses';
 
 export const ROUTES: Routes = [
-	{path: '', component: HomeComponent},
-	{path: 'home', component: HomeComponent},
+	// {path: '', component: HomeComponent},
+	// {path: 'home', component: HomeComponent},
+	{path: '', component: CoursesComponent},
 	{path: '**', component: NoContentComponent},
 ];
